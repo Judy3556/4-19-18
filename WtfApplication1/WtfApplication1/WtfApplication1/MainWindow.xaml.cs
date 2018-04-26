d6cece4afeb0ee4839530b5200439e46568b706a
m305-14\w401 <1147663235@qq.com>
2018-04-26 18:04:17 +0800

    课堂进度完成

--- WtfApplication1/WtfApplication1/WtfApplication1/MainWindow.xaml.cs
+++ WtfApplication1/WtfApplication1/WtfApplication1/MainWindow.xaml.cs
@@ -52,13 +52,37 @@ namespace WtfApplication1
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            Text_Box1.Text = "勤加锻炼控制饮食，祝您有理想的身材";
+            Text_Box1.Text = "勤加锻炼控制饮食";
                 
         }
 
         private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
         {
             Text_Box1.Text = "好好保持";
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            WeighBox .Text = WeightBar.Value.ToString();
+        }
+
+        private void HeightBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            HeighBox.Text = HeightBar.Value.ToString();
+            double h = double.Parse(HeighBox.Text);
+            double w = double.Parse(WeighBox.Text);
+            double bmi = w / (h * h);
+            Answer.Text = bmi.ToString();
+        }
+
+        private void Answer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void HeighBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
         }
     }
 }

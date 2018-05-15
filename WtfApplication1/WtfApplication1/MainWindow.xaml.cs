617e4430c8bcdca6a2ae20aaaf33c58210e41c25
LAPTOP-IABSIHS6\pc-01 <1147663235@qq.com>
2018-05-15 16:39:10 +0800

    製作完成

--- WtfApplication1/WtfApplication1/MainWindow.xaml.cs
+++ WtfApplication1/WtfApplication1/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace WtfApplication1
     {
         string heightNumber;
         string weightNumber;
+        int choseNumber;
 
         public MainWindow()
         {
@@ -36,17 +37,19 @@ namespace WtfApplication1
 
         private void Title_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            //移動視窗
+            this.DragMove();
         }
 
         private void FormalBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            
+            choseNumber = 1;
         }
 
         private void AsianBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            choseNumber = 2;
         }
 
         private void weightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -55,7 +58,65 @@ namespace WtfApplication1
             {
                 //轉化拉杆數值
                 weightNumber = weightSlider.Value.ToString();
+                double weightWeight = (weightSlider.Value - weightSlider.Minimum) / (weightSlider.Maximum - weightSlider.Minimum);
+                double range = weightSlider.ActualWidth - weightSlider.ActualWidth;
+                double weightN = Math.Round(double.Parse(weightNumber));
+
+                //計算BMI值
+                double bmi = weightSlider.Value / Math.Pow(heigtSlider.Value / 100, 2);
+
+                //拆分小數與整數
+                string[] parts = Math.Round(bmi, 1).ToString().Split('.');
+
+                //顯示數值
+                weigtBlock.Text = weightN.ToString();
+                numberOne.Text = parts[0];
+                if (parts.Length > 1)
+                {
+                    numberTwo.Text = "." + parts[1];
+                }
+
+                //顯示體重範圍
+                if (choseNumber == 1)
+                {
+                    if (bmi <= 24.9 && bmi >= 18.5)
+                    {
+                        bmiText.Text = "您的體形為：正常";
+                    }
+                    else if (bmi < 18.5)
+                    {
+                        bmiText.Text = "您的體形為：偏瘦";
+                    }
+                    else if (bmi <= 30 && bmi > 24.9)
+                    {
+                        bmiText.Text = "您的體形為：超重";
+                    }
+                    else
+                    {
+                        bmiText.Text = "您的體形為：肥胖";
+                    }
+                }
+                else if (choseNumber == 2)
+                {
+                    if (bmi <= 22.9 && bmi >= 18.5)
+                    {
+                        bmiText.Text = "您的體形為：正常";
+                    }
+                    else if (bmi < 18.5)
+                    {
+                        bmiText.Text = "您的體形為：偏瘦";
+                    }
+                    else if (bmi <= 23 && bmi > 22.9)
+                    {
+                        bmiText.Text = "您的體形為：超重";
+                    }
+                    else
+                    {
+                        bmiText.Text = "您的體形為：肥胖";
+                    }
+                }
             }
+
 
         }
 
@@ -68,13 +129,77 @@ namespace WtfApplication1
 
                 double heightWeight = (heigtSlider.Value - heigtSlider.Minimum) / (heigtSlider.Maximum - heigtSlider.Minimum);
                 double range = heigtSlider.ActualWidth - heigtSlider.ActualWidth;
+                double heightN = Math.Round(double.Parse(heightNumber));
+
+                //計算BMI值
                 double bmi = weightSlider.Value / Math.Pow(heigtSlider.Value / 100, 2);
+
+                //拆分小數與整數
+                string[] parts = Math.Round(bmi, 1).ToString().Split('.');
+
+                //顯示數值
+                heightBlock.Text = heightN.ToString();
+                numberOne.Text = parts[0];
+                if (parts.Length > 1)
+                {
+                    numberTwo.Text ="."+ parts[1];
+                }
+
+                //顯示體重範圍
+                if (choseNumber == 1)
+                {
+                    if (bmi <= 24.9 && bmi >=18.5)
+                    {
+                        bmiText.Text = "您的體形為：正常";
+                    }
+                    else if (bmi < 18.5)
+                    {
+                        bmiText.Text = "您的體形為：偏瘦";
+                    }
+                    else if(bmi<=30&&bmi>24.9)
+                    {
+                        bmiText.Text = "您的體形為：超重";
+                    }
+                    else
+                    {
+                        bmiText.Text = "您的體形為：肥胖";
+                    }
+                }
+                else if (choseNumber == 2)
+                {
+                    if (bmi <= 22.9 && bmi >= 18.5)
+                    {
+                        bmiText.Text = "您的體形為：正常";
+                    }
+                    else if (bmi < 18.5)
+                    {
+                        bmiText.Text = "您的體形為：偏瘦";
+                    }
+                    else if (bmi <= 23 && bmi > 22.9)
+                    {
+                        bmiText.Text = "您的體形為：超重";
+                    }
+                    else
+                    {
+                        bmiText.Text = "您的體形為：肥胖";
+                    }
+                }
+                
+                
             }
         }
 
         private void closeBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            //建立關閉程式代碼
             this.Close();
+        }
+
+        private void returnBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //重置按鈕
+           heigtSlider.Value = heigtSlider.Minimum;
+            weightSlider.Value = weightSlider.Minimum;
         }
     }
 }
